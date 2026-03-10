9dced1a6962769da286cc02264d1944a3a5857f0
Eri R Jorge <erirjorge@gmail.com>
2026-03-10 15:06:06 -0400

    formated

--- tests/navigation.spec copy.ts
+++ tests/navigation.spec copy.ts
@@ -7,23 +7,8 @@ test("homepage has playwright in title", async ({ page }) => {
 
 test("get started link navigates to intro page", async ({ page }) => {
 	await page.goto("https://playwright.dev/");
+	await expect(page.locator("text=Get Started")).toBeVisible();
 	await page.click("text=Get Started");
 	await expect(page).toHaveURL("https://playwright.dev/docs/intro");
-});
-
-test('navigates to intro page when clicking "Get Started"', async ({
-	page,
-}) => {
-	await page.goto("https://playwright.dev/");
-	await page.click("text=Get Started");
-	await expect(page).toHaveURL("https://playwright.dev/docs/intro");
-});
-
-test("Scrolls to the bottom of the page and clicks on the last link", async ({
-	page,
-}) => {
-	await page.goto("https://playwright.dev/");
-	await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-	await page.click("text=GitHub");
-	await expect(page).toHaveURL("https://playwright.dev/");
+	await expect(page.locator("h1")).toHaveText("Installation");
 });
